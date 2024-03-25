b272d8bfebbac6fdc6c29917c8171be132aa5747
ji min <142903244+gggimmmin@users.noreply.github.com>
2024-03-25 22:47:11 +0900

    [feat] 페이지에 random quote 생성 구현

--- js/quotes.js
+++ js/quotes.js
@@ -42,3 +42,11 @@ const quotes = [
     author: "헨리 포드",
   },
 ];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
